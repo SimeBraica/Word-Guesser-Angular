fd58bfa75132d02ac78b9bc767a9ec3f4336c321
SimeBraica <sbraica21@foi.hr>
2024-08-20 15:37:14 +0200

    Create setup for components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WordContainerComponent } from './components/word-container/word-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordContainerComponent
   ],
   imports: [
     BrowserModule,
